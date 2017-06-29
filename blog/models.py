2f090e6899a5f10582c4eef3e85b91345a201d41
Sooram Kang <sooram@Sooramui-MacBook-Pro.local>
2017-06-30 02:02:51 +0900

    Add category and tag model

--- blog/models.py
+++ blog/models.py
@@ -4,9 +4,11 @@ from django.utils import timezone
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
-    # category = models.ForeignKey('categories.Category')
+    categories = models.ManyToManyField('Category')
+    tags = models.ManyToManyField('Tag', blank=True, null=True)
     title = models.CharField(max_length=200)
-    # image= models.ImageField()
+    # slug = models.SlugField(max_length=200)
+    image= models.ImageField(upload_to='uploads/', blank=True, null=True)
     text = models.TextField()
     created_date = models.DateTimeField(
             default=timezone.now)
@@ -20,3 +22,32 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+
+    class Meta: 
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.title
+
+    # def get_absolute_url(self):
+    #     return "/categories/%s/" % self.slug
+
+class Tag(models.Model):
+    slug = models.SlugField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.slug
+
+class Comment(models.Model):
+    author = models.CharField(max_length=100)
+    text = models.TextField()
+    post = models.ForeignKey('Post')
+    created_date = models.DateTimeField(
+            default=timezone.now)
+
+    def __unicode__(self):
+        return '%s' % self.author
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from .models import Post
+from .models import Post, Category
 from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    # categories = Category.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
@@ -39,6 +40,7 @@ def post_edit(request, pk):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 def books_home(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    category = Category.objects.get(title="Read")
+    posts = Post.objects.filter(categories=category).order_by('published_date')
     return render(request, 'blog/books_home.html', {'posts': posts})
 
